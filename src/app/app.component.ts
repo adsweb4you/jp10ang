b9d211ad8283be53e7467c633b9f4736dbefda5a
adsweb4you <77510467+adsweb4you@users.noreply.github.com>
2022-03-15 18:09:00 +0400

    reps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
+  // template:'<h1> aslkdjaskld </h1>',
+  // styles:['h1{background-color:red; color:white}']
 })
 export class AppComponent {
-  title = 'jp10ang';
+  title = 6;
+  colors = "green";
+  img = "assets/img/764824736.jpg"
+
+
+  changecolor(){
+   this.colors = this.colors == "green" ? 'red' : 'green'
+  }
+
 }
